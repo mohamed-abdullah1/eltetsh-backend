fc6129c8bc8d2b5ab41b88f68be72cc604e26398
mohamed-abdullah1 <melsayed506sa@gmail.com>
2024-03-13 01:44:43 +0200

    init

--- routes/users.router.js
+++
@@ -1,7 +0,0 @@
-const { registerUser, loginUser } = require("../controllers/users.controller");
-
-const router = require("express").Router();
-
-router.route("/register").post(registerUser);
-router.route("/login").post(loginUser);
-module.exports = router;
