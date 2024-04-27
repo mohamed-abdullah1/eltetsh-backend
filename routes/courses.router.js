76f613d606eac6048c22cdde31a4eae03e341fb6
mohamed-abdullah1 <melsayed506sa@gmail.com>
2024-04-27 22:21:07 +0300

    allow all get courses

--- routes/courses.router.js
+++ routes/courses.router.js
@@ -10,7 +10,7 @@ const { verifyAdmin, verifyToken } = require("../middleware/auth.middleware");
 const router = require("express").Router();
 
 router.route("/create").post(verifyAdmin, createCourse);
-router.route("/all").get(verifyAdmin, getAllCourses);
+router.route("/all").get(verifyToken, getAllCourses);
 router
   .route("/:id")
   .get(verifyToken, getCourseById)
