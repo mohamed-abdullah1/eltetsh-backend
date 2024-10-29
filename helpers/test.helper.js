ab019ae2e4f5da76953941d8a40fc5110159e757
mohamed-abdullah1 <melsayed506sa@gmail.com>
2024-10-29 18:51:03 +0300

    edit test route

--- helpers/test.helper.js
+++ helpers/test.helper.js
@@ -2,7 +2,8 @@ const { verifyAdmin } = require("../middleware/auth.middleware");
 
 const router = require("express").Router();
 
-const test = (req, res) => res.status(200).json({ msg: "server is live 🖖🏼" });
+// const test = (req, res) => res.status(200).json({ msg: "server is live 🖖🏼" });
+const test = (req, res) => "HELLO WORLD";
 
 // router.get("/", verifyAdmin, test);
 router.get("/", test);
